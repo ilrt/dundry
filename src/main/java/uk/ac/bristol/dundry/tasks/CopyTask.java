d19e4a90b6200966691746cb9db3e0afa093b989
Damian <d.steer@bris.ac.uk>
2012-04-18 16:57:27 +0100

    Make tree copy in data store. Use DC vocab for this.

--- src/main/java/uk/ac/bristol/dundry/tasks/CopyTask.java
+++ src/main/java/uk/ac/bristol/dundry/tasks/CopyTask.java
@@ -1,13 +1,15 @@
 package uk.ac.bristol.dundry.tasks;
 
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.vocabulary.DCTerms;
+import com.hp.hpl.jena.vocabulary.DCTypes;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import org.quartz.JobDataMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,13 +58,17 @@ public class CopyTask extends JobBase {
         try {
             return Files.walkFileTree(from, new SimpleFileVisitor<Path>() {
                 
+                // Keep track of parents as we walk the tree, so
+                // tree structure is recorded faithfully
+                Deque<Resource> parents = new ArrayDeque<>();
+                                
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     Path rel = parent.relativize(file);
                     log.debug("Visit {}", file);
                     Files.copy(file, to.resolve(rel), StandardCopyOption.COPY_ATTRIBUTES);
                     
-                    recordVisit(rel);
+                    recordVisit(rel, RDFS.Resource); // we really don't know what it is
                     
                     return FileVisitResult.CONTINUE;
                 }
@@ -74,15 +80,34 @@ public class CopyTask extends JobBase {
                     log.trace("Create dir {}", to.resolve(rel));
                     Files.createDirectory(to.resolve(rel));
                     
-                    recordVisit(rel);
+                    // Record dir as a collection of resources
+                    Resource item = recordVisit(rel, DCTypes.Collection);
+                    parents.push(item); // make this the current parent
                     
                     return FileVisitResult.CONTINUE;
                 }
                 
-                private void recordVisit(Path relativePath) {
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exception) {
+                    
+                    parents.pop(); // finished. Forget this directory
+                    
+                    return FileVisitResult.CONTINUE;
+                } 
+                
+                /**
+                 * Make a note of this item in the database
+                 * @return item in database
+                 */
+                private Resource recordVisit(Path relativePath, Resource type) {
+                    // Parent is either top of stack or -- if absent -- root
+                    Resource parent = (parents.isEmpty()) ? root : parents.peek() ;
+                    
                     Resource item = root.getModel().createResource(root.getURI() + "/" + relativePath.toString());
-                    item.addProperty(DCTerms.type, "file");
-                    root.addProperty(DCTerms.hasPart, item);
+                    if (!RDFS.Resource.equals(type)) item.addProperty(RDF.type, type);
+                    
+                    parent.addProperty(DCTerms.hasPart, item);
+                    return item;
                 }
             });
         } catch (IOException ex) {
