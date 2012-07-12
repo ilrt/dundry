e52bba590b07b64a91fcfd506d8185fd2abd0718
Damian Steer <pldms@mac.com>
2012-07-12 01:05:24 +0100

    Tidy torrent creation

--- src/main/java/uk/ac/bristol/dundry/tasks/MakeTorrentTask.java
+++ src/main/java/uk/ac/bristol/dundry/tasks/MakeTorrentTask.java
@@ -9,6 +9,7 @@ import java.net.URISyntaxException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.security.NoSuchAlgorithmException;
@@ -23,29 +24,33 @@ import uk.ac.bristol.dundry.dao.Repository;
  * @author Damian Steer <d.steer@bris.ac.uk>
  */
 public class MakeTorrentTask extends JobBase {
-    
+
     @Override
     public void execute(Repository repo, Resource item, Resource prov, String id,
-        Path root, JobDataMap jobData) throws JobExecutionException {
-        
-        Path publishedPath = repo.getPublishPathForId(id);
-        String tracker = jobData.getString("torrent.tracker");
+            Path root, JobDataMap jobData) throws JobExecutionException {
         try {
-            Torrent torrent = Torrent.create(
-                    publishedPath.toFile(), listContent(publishedPath), 
-                    new URI(tracker), "dundry");
-            File torrentFile = publishedPath.resolve(id + ".torrent").toFile();
-            torrent.save(torrentFile);
-        } catch (NoSuchAlgorithmException | InterruptedException | IOException | URISyntaxException ex) {
-            throw new JobExecutionException("Creating torrent failed", ex);
+            makeTorrentFile(repo.getPublishPathForId(id),
+                    repo.getPublishPathForId(id).resolve(id + ".torrent"),
+                    jobData.getString("torrent.tracker"));
+        } catch (NoSuchAlgorithmException | InterruptedException |
+                IOException | URISyntaxException ex) {
+            throw new JobExecutionException("Issue creating torrent", ex);
         }
     }
-    
+
+    protected void makeTorrentFile(Path toShare, Path torrentFile, String tracker) throws NoSuchAlgorithmException, InterruptedException, IOException, URISyntaxException {
+        Torrent torrent = Torrent.create(
+                toShare.toFile(), listContent(toShare),
+                new URI(tracker), "dundry");
+        torrent.save(torrentFile.toFile());
+    }
+
     private List<File> listContent(final Path parent) throws IOException {
-        
+
         final List<File> files = new ArrayList<>();
-        
+
         Files.walkFileTree(parent, new SimpleFileVisitor<Path>() {
+
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 files.add(file.toFile());
@@ -53,5 +58,11 @@ public class MakeTorrentTask extends JobBase {
             }
         });
         return files;
+    }
+
+    public static void main(String... args) throws NoSuchAlgorithmException, InterruptedException, IOException, URISyntaxException {
+        MakeTorrentTask m = new MakeTorrentTask();
+        
+        m.makeTorrentFile(Paths.get("/tmp/test-dir"), Paths.get("/tmp/test.torrent"), "http://example.com/tracker");
     }
 }
