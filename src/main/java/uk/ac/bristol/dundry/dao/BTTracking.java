a6932cf62dd7797338a6ed0a6970135a4106b870
Damian Steer <pldms@mac.com>
2012-07-15 01:19:06 +0100

    BT nearly working?

--- src/main/java/uk/ac/bristol/dundry/dao/BTTracking.java
+++ src/main/java/uk/ac/bristol/dundry/dao/BTTracking.java
@@ -1,6 +1,16 @@
 package uk.ac.bristol.dundry.dao;
 
+import com.turn.ttorrent.client.Client;
+import com.turn.ttorrent.client.SharedTorrent;
+import com.turn.ttorrent.common.Torrent;
 import com.turn.ttorrent.tracker.TrackedTorrent;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import org.slf4j.Logger;
@@ -8,50 +18,110 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
- * Tracks torrents and purges clients.
- * TTorrent implements this, of course, but uses its own http server.
- * 
+ * Tracks trackedTorrents and purges clients. TTorrent implements this, of course, but
+ * uses its own http server.
+ *
  * @author Damian Steer <d.steer@bris.ac.uk>
  */
-@Component
 public class BTTracking {
-    
+
     final static Logger log = LoggerFactory.getLogger(BTTracking.class);
-    
     final Thread purger;
-    
-    private final ConcurrentMap<String, TrackedTorrent> torrents;
-    
-    public BTTracking() {
-        torrents = new ConcurrentHashMap<>();
-        purger = new Thread(new Purger(torrents), "Torrent client purger");
+    private final ConcurrentMap<String, TrackedTorrent> trackedTorrents;
+    private final ConcurrentMap<String, Client> clients;
+    private final InetAddress address;
+
+    public BTTracking(String publishBase) throws IOException, NoSuchAlgorithmException {
+        log.info("Starting up bittorrent");
+        address = InetAddress.getLocalHost();
+        log.info("Address is {}", address.getHostAddress());
+        trackedTorrents = new ConcurrentHashMap<>();
+        clients = new ConcurrentHashMap<>();
+        initTorrents(Paths.get(publishBase), trackedTorrents, clients);
+        purger = new Thread(new Purger(trackedTorrents), "Torrent client purger");
         purger.start(); // may not be a good idea? Could quartz do this work instead?
     }
-
+    
     public ConcurrentMap<String, TrackedTorrent> getTorrents() {
-        return torrents;
+        return trackedTorrents;
     }
     
-    static class Purger implements Runnable {
-        private final ConcurrentMap<String, TrackedTorrent> torrents;
+    /**
+     * Look for, load, and serve trackedTorrents in base
+     * @param base
+     * @param map
+     * @return 
+     */
+    private void initTorrents(Path base, 
+            ConcurrentMap<String, TrackedTorrent> trackedTorrents,
+            ConcurrentMap<String, Client> clients)
+                throws IOException, NoSuchAlgorithmException {
+        log.info("Looking for existing torrents in {}", base);
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(base)) {
+            for (Path pubDir: stream) {
+                // Find a torrent for this dir if possible
+                Path torrentFile = findTorrent(pubDir, pubDir, base);
+                
+                if (torrentFile != null) {
+                    log.info("Found torrent {}", torrentFile);
+                    
+                    // Load as a seeder
+                    Torrent torrent = Torrent.load(torrentFile.toFile(), base.toFile(), true);
+                    
+                    SharedTorrent shared = new SharedTorrent(torrent, base.toFile(), true);
+                    
+                    if (!shared.isSeeder()) {
+                        log.warn("Torrent {} is not seeding. Skipping.", torrentFile);
+                        continue;
+                    }
+                    
+                    TrackedTorrent tracked = new TrackedTorrent(torrent);
+                    
+                    trackedTorrents.put(torrent.getHexInfoHash(), tracked);
+                    
+                    Client client = new Client(address, shared);
+                    clients.put(torrent.getHexInfoHash(), client);
+                    client.share();
+                    
+                    log.info("Sharing {}", torrentFile);
+                } 
+            }
+        }
+    }
+
+    private Path findTorrent(Path forDir, Path... locations) {
+        Path toFind = Paths.get(forDir.getFileName().toString() + ".torrent");
         
+        for (Path location: locations) {
+            Path candidate = location.resolve(toFind);
+            if (Files.exists(candidate)) return candidate;
+        }
+        
+        return null;
+    }
+
+    static class Purger implements Runnable {
+
+        private final ConcurrentMap<String, TrackedTorrent> torrents;
+
         Purger(ConcurrentMap<String, TrackedTorrent> torrents) {
             this.torrents = torrents;
         }
 
         @Override
         public void run() {
-            try {
-                Thread.sleep(30 * 1000);
-            } catch (InterruptedException ex) {
-                log.error("Purger sleep disturbed", ex);
-            }
-            
-            log.info("checking unfresh peers");
-            for (TrackedTorrent torrent: torrents.values()) {
-                torrent.collectUnfreshPeers();
+            while (true) {
+                try {
+                    Thread.sleep(30 * 1000);
+                } catch (InterruptedException ex) {
+                    log.error("Purger sleep disturbed", ex);
+                }
+
+                log.info("checking unfresh peers");
+                for (TrackedTorrent torrent : torrents.values()) {
+                    torrent.collectUnfreshPeers();
+                }
             }
         }
-        
     }
 }
--- src/main/java/uk/ac/bristol/dundry/webresources/BTTracker.java
+++ src/main/java/uk/ac/bristol/dundry/webresources/BTTracker.java
@@ -4,14 +4,29 @@
  */
 package uk.ac.bristol.dundry.webresources;
 
+import com.turn.ttorrent.common.protocol.TrackerMessage;
+import com.turn.ttorrent.common.protocol.TrackerMessage.AnnounceRequestMessage.RequestEvent;
+import com.turn.ttorrent.common.protocol.TrackerMessage.ErrorMessage.FailureReason;
+import com.turn.ttorrent.common.protocol.TrackerMessage.MessageValidationException;
+import com.turn.ttorrent.common.protocol.http.HTTPAnnounceResponseMessage;
+import com.turn.ttorrent.common.protocol.http.HTTPTrackerErrorMessage;
+import com.turn.ttorrent.tracker.TrackedPeer;
+import com.turn.ttorrent.tracker.TrackedTorrent;
 import com.turn.ttorrent.tracker.TrackerFactory;
 import com.turn.ttorrent.tracker.TrackerService;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.concurrent.ConcurrentMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.ac.bristol.dundry.dao.BTTracking;
@@ -38,24 +53,55 @@ public class BTTracker {
         TrackerService ts = TrackerFactory.getTrackerService("Dundry (TTorrent)", torrentTracker.getTorrents());
         SFContainerAdapter.handle(ts, request, response);
     }*/
-    
-    enum Event { started, completed, stopped, none }
-    
+        
     /** See http://wiki.theory.org/BitTorrent_Tracker_Protocol#Basic_Tracker_Announce_Request **/
     @GET
     @Path("announce")
-    public void track(
-        @QueryParam("info_hash") String info_hash, 
-        @QueryParam("peer_id") String peer_id,
+    public Response track(
+        @QueryParam("info_hash") String infoHash, 
+        @QueryParam("peer_id") String peerId,
         @QueryParam("ip") @DefaultValue("") String ip, 
         @QueryParam("port") int port,
         @QueryParam("uploaded") long uploaded, 
         @QueryParam("downloaded") long downloaded,
         @QueryParam("left") long left, 
-        @QueryParam("event") @DefaultValue("none") Event event,
-        @QueryParam("numwant") @DefaultValue("50") int numwant
-            ) {
+        @QueryParam("event") @DefaultValue("none") RequestEvent event,
+        @QueryParam("numwant") @DefaultValue("50") int numwant,
+        @QueryParam("compact") @DefaultValue("0") int compact,
+        @QueryParam("nopeerid") @DefaultValue("0") int noPeerId
+            ) throws UnsupportedEncodingException, IOException, MessageValidationException {
         
+        ConcurrentMap<String, TrackedTorrent> trackedTorrents = torrentTracker.getTorrents();
+                
+        if (!trackedTorrents.containsKey(infoHash)) {
+            return error(Response.Status.BAD_REQUEST, FailureReason.UNKNOWN_TORRENT);
+        }
+        
+        TrackedTorrent tt = trackedTorrents.get(infoHash);
+        
+        TrackedPeer peer = tt.update(
+                            TrackerMessage.AnnounceRequestMessage.RequestEvent.NONE,
+                            ByteBuffer.wrap(peerId.getBytes("US-ASCII")), 
+                            peerId, ip, port, uploaded, downloaded, left);
+        
+        HTTPAnnounceResponseMessage response = HTTPAnnounceResponseMessage.craft(
+				tt.getAnnounceInterval(),
+				TrackedTorrent.MIN_ANNOUNCE_INTERVAL_SECONDS,
+				"Dundry (TTorrent)",
+				tt.seeders(),
+				tt.leechers(),
+				tt.getSomePeers(peer));
+        
+        return Response.ok(response.getData(), "text/plain")
+                .header("Date", System.currentTimeMillis())
+                .header("Server", "Dundry (TTorrent)")
+                .build();
+    }
+
+    private Response error(Status status, FailureReason reason)
+            throws IOException, MessageValidationException {
+        HTTPTrackerErrorMessage message = HTTPTrackerErrorMessage.craft(reason);
+        return Response.status(status).entity(message.getData()).build();
     }
 }
 
