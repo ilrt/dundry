b71b979b68966f74681eb18225b937653153224b
Damian <d.steer@bris.ac.uk>
2012-07-16 17:33:32 +0100

    Bittorrent cleaned up a little. Working ok, pace ipv6 issue

--- src/main/java/uk/ac/bristol/dundry/webresources/BTTracker.java
+++ src/main/java/uk/ac/bristol/dundry/webresources/BTTracker.java
@@ -1,13 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package uk.ac.bristol.dundry.webresources;
 
+import com.coremedia.iso.Hex;
 import com.google.common.base.Joiner;
-import com.turn.ttorrent.bcodec.BDecoder;
-import com.turn.ttorrent.bcodec.BEValue;
-import com.turn.ttorrent.common.protocol.TrackerMessage;
 import com.turn.ttorrent.common.protocol.TrackerMessage.AnnounceRequestMessage.RequestEvent;
 import com.turn.ttorrent.common.protocol.TrackerMessage.ErrorMessage.FailureReason;
 import com.turn.ttorrent.common.protocol.TrackerMessage.MessageValidationException;
@@ -15,17 +9,14 @@ import com.turn.ttorrent.common.protocol.http.HTTPAnnounceResponseMessage;
 import com.turn.ttorrent.common.protocol.http.HTTPTrackerErrorMessage;
 import com.turn.ttorrent.tracker.TrackedPeer;
 import com.turn.ttorrent.tracker.TrackedTorrent;
-import com.turn.ttorrent.tracker.TrackerFactory;
-import com.turn.ttorrent.tracker.TrackerService;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 import java.net.URLDecoder;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -33,14 +24,12 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.ac.bristol.dundry.dao.BTTracking;
 import uk.ac.bristol.dundry.dao.Repository;
-import uk.ac.bristol.dundry.webresources.adapter.SFContainerAdapter;
 
 /**
  *
@@ -52,6 +41,8 @@ public class BTTracker {
    
     final static Logger log = LoggerFactory.getLogger(BTTracker.class);
     
+    final static Charset ISO88591 = Charset.forName("iso-8859-1");
+    
     @Autowired
     BTTracking torrentTracker;
     
@@ -62,8 +53,8 @@ public class BTTracker {
     @GET
     @Path("announce")
     public Response track(
-        @QueryParam("info_hash") String infoHash, 
-        @QueryParam("peer_id") String peerId,
+        @QueryParam("info_hash") String infoHashBroken, 
+        @QueryParam("peer_id") String peerIdBroken,
         @QueryParam("ip") @DefaultValue("") String ip, 
         @QueryParam("port") int port,
         @QueryParam("uploaded") long uploaded, 
@@ -73,27 +64,29 @@ public class BTTracker {
         @QueryParam("numwant") @DefaultValue("50") int numwant,
         @QueryParam("compact") @DefaultValue("0") int compact,
         @QueryParam("nopeerid") @DefaultValue("0") int noPeerId,
-        @Context UriInfo uriInfo
+        @Context HttpServletRequest req
             ) throws UnsupportedEncodingException, IOException, MessageValidationException {
         
         log.debug("info {} peer {} ip {} port {} up {} down {} left {} event {} numwant {} compact {} nopeerid {}",
-                new Object[]{infoHash, peerId, ip, port, uploaded, downloaded, left, eventName, numwant, compact, noPeerId});
+                new Object[]{infoHashBroken, peerIdBroken, ip, port, uploaded, downloaded, left, eventName, numwant, compact, noPeerId});
+         
+        // Fix up request params
         
         RequestEvent event = RequestEvent.valueOf(eventName.toUpperCase());
         
+        // Jersey and BT disagree about string decoding
+        String rawQuery = req.getQueryString();
+        String infoHash = Hex.encodeHex(getParam("info_hash", rawQuery));
+        byte[] peerId = getParam("peer_id", rawQuery);
+        String peerIdHash = Hex.encodeHex(peerId);
+        
+        if (ip.isEmpty()) {
+            // TODO: I seem to have an ipv6 issue with loopback
+            ip = req.getRemoteHost();
+            log.debug("IP for {} is empty, using {}", peerIdHash, ip);
+        }
+        
         ConcurrentMap<String, TrackedTorrent> trackedTorrents = torrentTracker.getTorrents();
-        
-        
-        // infoHash is encoded in an annoying way that jersey messes up
-        String rawQuery = uriInfo.getRequestUri().getRawQuery();
-        int start = rawQuery.indexOf("info_hash=") + 10; // plus length of param bit
-        String encodedHash = rawQuery.substring(start,rawQuery.indexOf('&', start));
-        byte[] hashBytes = URLDecoder.decode(encodedHash, "iso-8859-1").getBytes("iso-8859-1");
-        infoHash = new BigInteger(hashBytes).toString(16).toUpperCase();
-        
-        log.warn("encodedHash: {} infoHash: {}", encodedHash, infoHash);
-        
-        String peerIdHash = new BigInteger(peerId.getBytes("iso-8859-1")).toString(16);
         
         if (!trackedTorrents.containsKey(infoHash)) {
             String torrents = Joiner.on(" , ").join(trackedTorrents.keySet());
@@ -106,16 +99,15 @@ public class BTTracker {
         Map<String, TrackedPeer> peers = tt.getPeers();
         if (!peers.containsKey(peerIdHash) && event != RequestEvent.STARTED) {
             String allPeers = Joiner.on(" , ").join(peers.keySet());
-            log.warn("Peer {} is unknown. I know [{}].", peerId, allPeers);
+            log.warn("Peer {} is unknown. I know [{}].", peerIdHash, allPeers);
             
+            // I think we seem sometimes miss started events?
             if (event == RequestEvent.NONE) event = RequestEvent.STARTED;
         }
-        
-        log.warn("Event: {}", event);
-                
+                        
         TrackedPeer peer = tt.update(
                             event,
-                            ByteBuffer.wrap(peerId.getBytes("iso-8859-1")), 
+                            ByteBuffer.wrap(peerId), 
                             peerIdHash, ip, port, uploaded, downloaded, left);
         
         HTTPAnnounceResponseMessage response = HTTPAnnounceResponseMessage.craft(
@@ -136,6 +128,26 @@ public class BTTracker {
             throws IOException, MessageValidationException {
         HTTPTrackerErrorMessage message = HTTPTrackerErrorMessage.craft(reason);
         return Response.status(status).entity(message.getData()).build();
+    }
+    
+    /**
+     * Bittorrent treats some query param fields as raw bytes, which conflicts
+     * mightily with jersey
+     * @param paramName 
+     * @param rawQueryString
+     * @return 
+     */
+    private byte[] getParam(String paramName, String rawQueryString) throws UnsupportedEncodingException {
+        
+        // Get the value of interest
+        int start = rawQueryString.indexOf(paramName) + 1 + paramName.length();
+        int end = rawQueryString.indexOf('&', start);
+        String encodedValue = (end == -1) ?
+                rawQueryString.substring(start) : 
+                rawQueryString.substring(start, end);
+        
+        // Decode using iso 8859 1, which will pull values out simply
+        return URLDecoder.decode(encodedValue, "iso-8859-1").getBytes(ISO88591);
     }
 }
 
