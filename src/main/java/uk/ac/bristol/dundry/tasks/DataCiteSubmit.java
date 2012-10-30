d00bfe408ad1edb3b541ed853d5a3929032f65d0
Damian <d.steer@bris.ac.uk>
2012-10-30 17:12:58 +0000

    Write datacite record from rdf

--- src/main/java/uk/ac/bristol/dundry/tasks/DataCiteSubmit.java
+++ src/main/java/uk/ac/bristol/dundry/tasks/DataCiteSubmit.java
@@ -2,13 +2,20 @@ package uk.ac.bristol.dundry.tasks;
 
 import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.sparql.core.DatasetImpl;
+import com.hp.hpl.jena.vocabulary.DCTerms;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -100,5 +107,69 @@ public class DataCiteSubmit extends JobBase {
         prov.getModel().write(System.out, "TTL");
         System.out.println("========== item =========");
         item.getModel().write(System.out, "TTL");
+    }
+    
+    // Write item out in datacite format
+    public void toDataCite(Resource item, String doi, XMLStreamWriter writer) throws XMLStreamException {
+        writer.writeStartDocument();
+        
+        // Preamble
+        writer.setDefaultNamespace("http://datacite.org/schema/kernel-2.2");
+        writer.writeStartElement("resource");
+        writer.writeDefaultNamespace("http://datacite.org/schema/kernel-2.2");
+        writer.writeAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        writer.writeAttribute("xsi:schemaLocation", "http://datacite.org/schema/kernel-2.2 http://schema.datacite.org/meta/kernel-2.2/metadata.xsd");
+        
+        // Titles
+        writer.writeStartElement("titles");
+        write(item, DCTerms.title, "title", writer);
+        write(item, DCTerms.alternative, "title", writer, "titleType", "Alternative");
+        writer.writeEndElement();
+        
+        // Description
+        writer.writeStartElement("descriptions");
+        write(item, DCTerms.description, "description", writer);
+        writer.writeEndElement();
+        
+        
+        
+        // Close root and document
+        writer.writeEndElement();
+        writer.writeEndDocument();
+        
+        writer.flush();
+    }
+    
+    /**
+     * Write out rdf values, if present, to XML stream
+     * 
+     * @param item Resource being written
+     * @param property Property to write
+     * @param element XML element to use
+     * @param writer XML stream to write to
+     * @param attVals Addition attribute values to include
+     */
+    private void write(Resource item, Property property, String element, 
+            XMLStreamWriter writer, String... attVals) throws XMLStreamException {
+        StmtIterator si = item.listProperties(property);
+        while (si.hasNext()) {
+            writer.writeStartElement(element);
+            // Write attributes out
+            for (int i = 0; i < attVals.length; i+=2) {
+                writer.writeAttribute(attVals[i], attVals[i+1]);
+            }
+            
+            // Write value
+            writer.writeCharacters(toString(si.next().getObject()));
+            
+            writer.writeEndElement();
+        }
+    }
+
+    
+    private String toString(RDFNode object) {
+        if (object.isURIResource()) return object.asResource().getURI();
+        else if (object.isLiteral()) return object.asLiteral().getLexicalForm();
+        else return "";
     }
 }
